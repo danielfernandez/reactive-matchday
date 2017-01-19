2e83d3402258d2664f83fd862b179a40c29ad66e
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-01-20 00:38:23 +0100

    Initialised basketflux-web

---
+++ basketflux-web/src/main/java/com/danielfernandez/basketflux/BasketfluxWebApplication.java
@@ -0,0 +1,12 @@
+package com.danielfernandez.basketflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketfluxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketfluxWebApplication.class, args);
+	}
+}
---
+++ basketflux-web/src/test/java/com/danielfernandez/basketflux/BasketfluxWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.danielfernandez.basketflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasketfluxWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
